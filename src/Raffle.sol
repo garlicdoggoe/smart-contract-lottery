bb47d3110a6571f1d6a1f3078f0ee40032c917d6
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-10 14:42:37 +0800

    update: refactor code to follow CEI methodology

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -39,6 +39,7 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
     /** Events */
     event RaffleEntered(address indexed player);
+    emit WinnerPicked(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -105,15 +106,25 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    // need to define
+    // need to define from an abstract contract
+    // CEI: Checks-Effects-Interactions
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
     ) internal override {
+        // checks
+        // requires, conditionals
+        
+        // effect (internal contract state)
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
         s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(s_recentWinner);
+        
+        // interactions (external contracts interactions)
         (bool success, ) = recentWinner.call{value: address(this).balance}(""); // give the whole pool to the recent winner
         if (!success) {
             revert Raffle__TransferFailed();
