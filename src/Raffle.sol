d9f45aa7f620668ce5964b0b4dc6afc07f69a0ee
garlicdoggoe <garlicspoofiyyy@gmail.com>
2025-06-10 14:09:24 +0800

    add: chainlink vrf for randomness

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,22 +1,104 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Lottery Smart Contract
  * @author Selwyn Guiruela
  * @notice This contract is for creating a simple raffle
  * @dev Implements Chainlink VRF v2.5 for randomness
  */
-contract Raffle {
-    uint256 private immutable i_entranceFee;
+contract Raffle is VRFConsumerBaseV2Plus {
+    /** Errors */
+    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
 
-    constructor(uint256 entranceFee) {
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players; // since whoever will win the lottery, we neet to send the prize, we use payable
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+
+    /** Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        uint256 subscriptionId,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        // whenever we update something in storage, we need to create an event
+        // 1. Makes migration easier
+        // 2. Makes front end "indexing" easier
+        emit RaffleEntered(msg.sender);
+    }
 
-    function pickWinner() public {}
+    // 1. Get a random number
+    // 2. Use random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        // Get our random number from Chainlink VRF
+        // 1. Request RNG tx
+        // 2. Get RNG tx
+        // https://docs.chain.link/vrf/v2-5/getting-started
+        // This is the struct object that we need to pass for the requestID
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+    }
+
+    // need to define
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}(""); // give the whole pool to the recent winner
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 
     /** Getter Functions */
     function getEntranceFee() external view returns (uint256) {
